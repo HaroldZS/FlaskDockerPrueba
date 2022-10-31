4d576d6d28ed421ca2791fa3e589e0280a333d0e
HaroldZS <haroldzs@gmail.com>
2022-10-30 23:53:14 -0400

    Templates de Flask

--- src/app.py
+++ src/app.py
@@ -1,15 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 from users import users
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def ping():
-    return jsonify({"response":"hello world"})
+    return render_template('raiz.html', raiz = "hello world")
 
 @app.route('/users')
 def usersHandler():
-    return jsonify({"users":users})
+    return render_template('usuarios.html', usuarios = users)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=4000, debug=True)
--- src/users.py
+++ src/users.py
@@ -1,5 +1,37 @@
 users = [
-    {"name":"Fazt"},
-    {"name":"John"},
-    {"name":"Ryan"}
+    {"name":"Harold Zurita",
+    "edad": 24,
+    "ciudad": "Cochabamba",
+    "carrera": "Ing.Sistemas" 
+    },
+    {"name":"Emmanuel Mamani",
+    "edad": 22,
+    "ciudad": "Cochabamba",
+    "carrera": "Ing.Sistemas" 
+    },
+    {"name":"Ivan Monzón",
+    "edad": 22,
+    "ciudad": "Cochabamba",
+    "carrera": "Ing.Sistemas" 
+    },
+    {"name":"Dafne Cruz",
+    "edad": 21,
+    "ciudad": "Cochabamba",
+    "carrera": "Ing.Sistemas" 
+    },
+    {"name":"John Ponce",
+    "edad": 27,
+    "ciudad": "La Paz",
+    "carrera": "Ing.Civil" 
+    },
+    {"name":"Ryan Garcia",
+    "edad": 26,
+    "ciudad": "Sucre",
+    "carrera": "Ing.Industrial" 
+    },
+    {"name":"Kevin Montecinos",
+    "edad": 21,
+    "ciudad": "Santa Cruz",
+    "carrera": "Ing.Económica" 
+    }
 ]
